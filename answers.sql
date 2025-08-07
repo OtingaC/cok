d3fbf5a9fd20b4b77a51d9174c0544d7dcbaa93c
OtingaC <collinsotinga@gmail.com>
2025-08-07 06:14:31 +0300

    sql week2 assignment

--- answers.sql
+++ answers.sql
@@ -1,5 +1,20 @@
--- 1. Create salesDB
-CREATE DATABASE salesDB;
-
--- 2. Drop demo DB
-DROP DATABASE demo;
+--Retrieve from payments table
+SELECT checkNumber, paymentDate, amount FROM payments;
+--Retrieve orders that are 'In Process'. descending-orderDate
+SELECT orderDate, requiredDate, status
+FROM orders
+WHERE status = 'In Process'
+ORDER BY orderDate DESC;
+--'Sales Rep' as job title descending-employeeNumber
+SELECT firstName, lastName, email
+FROM employees
+WHERE jobTitle = 'SalesRep'
+ORDER BY employeeNumber DESC;
+--Retrieve all columns and records from office table
+SELECT *
+FROM offices;
+--Fetch productName and quantityinStock from products table ascending orde-buyPrice, limit output to 5records
+SELECT productName, quantityinStock
+FROM products
+ORDER BY buyPrice
+LIMIT 5;
