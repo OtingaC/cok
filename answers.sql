3398a1d389ab641d5a24da53907b9f182ac7262f
Collins <collinsotinga@gmail.com>
2025-08-15 11:05:33 +0300

    Week2 Database Assignment

--- answers.sql
+++ answers.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 --Retrieve from payments table
 SELECT checkNumber, paymentDate, amount FROM payments;
 --Retrieve orders that are 'In Process'. descending-orderDate
@@ -17,4 +18,25 @@ FROM offices;
 SELECT productName, quantityinStock
 FROM products
 ORDER BY buyPrice
+=======
+--Retrieve from payments table
+SELECT checkNumber, paymentDate, amount FROM payments;
+--Retrieve orders that are 'In Process'. descending-orderDate
+SELECT orderDate, requiredDate, status
+FROM orders
+WHERE status = 'In Process'
+ORDER BY orderDate DESC;
+--'Sales Rep' as job title descending-employeeNumber
+SELECT firstName, lastName, email
+FROM employees
+WHERE jobTitle = 'SalesRep'
+ORDER BY employeeNumber DESC;
+--Retrieve all columns and records from office table
+SELECT *
+FROM offices;
+--Fetch productName and quantityinStock from products table ascending orde-buyPrice, limit output to 5records
+SELECT productName, quantityinStock
+FROM products
+ORDER BY buyPrice
+>>>>>>> 5722852 (Week2 Database Assignment)
 LIMIT 5;
